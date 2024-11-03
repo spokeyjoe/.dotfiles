d659db5abc812f1feadf5a939f2e353c155d9192
spokeyjoe <hq11@rice.edu>
2024-11-03 17:38:50 -0600

    update neotree config

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,22 +1,22 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	version = "*",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	cmd = "Neotree",
-	keys = {
-		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
-	},
-	opts = {
-		filesystem = {
-			window = {
-				mappings = {
-					["\\"] = "close_window",
-				},
-			},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
 		},
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				popup_border_style = "rounded",
+				filesystem = {
+					hijack_netrw_behavior = "open_default",
+				},
+			})
+
+			vim.cmd([[nnoremap <leader>tf :Neotree toggle reveal_force_cwd<cr>]])
+		end,
 	},
 }
