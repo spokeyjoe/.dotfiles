1c14b32b37abcac0740d2cf54d99b23b0c4a806f
spokeyjoe <hq11@rice.edu>
2025-03-15 19:29:31 +0800

    update obsidian/neotree; delete avante

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -2,20 +2,10 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    requires = {
+    dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      close_if_last_window = true,
-      popup_border_style = "rounded",
-      filesystem = {
-        hijack_netrw_behavior = "open_default",
-        follow_current_file = {
-          enabled = true,
-        },
-      },
     },
   },
 }
--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -16,5 +16,32 @@ return {
         path = "~/Documents/notes",
       },
     },
+
+    daily_notes = {
+      folder = "notes/daily",
+      date_format = "%Y-%m-%d",
+      alias_format = "%B %-d, %Y",
+      default_tags = { "daily-notes" },
+      template = nil,
+    },
+
+    ---@return table
+    note_frontmatter_func = function(note)
+      if note.title then
+        note:add_alias(note.title)
+      end
+
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
+      return out
+    end,
   },
 }
