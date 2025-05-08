a0b60311a74e0df0354d6ac1341ca0a8579496dc
spokeyjoe <hq11@rice.edu>
2025-05-08 14:30:48 +0800

    migrate to community fork of obsidian.nvim

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,69 +1,134 @@
 return {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
-  event = {
-    "BufReadPre " .. vim.fn.expand "~" .. "/Documents/notes/*.md",
-    "BufNewFile " .. vim.fn.expand "~" .. "/Documents/notes/*.md",
-  },
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
   },
-  opts = {
-    workspaces = {
-      {
-        name = "notes",
-        path = "~/Documents/notes",
+
+  config = function()
+    require("obsidian").setup {
+      workspaces = {
+        {
+          name = "notes",
+          path = "~/Documents/notes",
+        },
       },
-    },
 
-    daily_notes = {
-      folder = "daily",
-      date_format = "%Y-%m-%d",
-      alias_format = "%B %-d, %Y",
-      default_tags = { "daily-notes" },
-      template = nil,
-    },
+      daily_notes = {
+        folder = "daily",
+        date_format = "%Y-%m-%d",
+        alias_format = "%B %-d, %Y",
+        default_tags = { "daily-notes" },
+        template = nil,
+        workdays_only = false,
+      },
 
-    wiki_link_func = "use_alias_only",
+      completion = {
+        -- Enables completion using nvim_cmp
+        nvim_cmp = true,
+        -- Enables completion using blink.cmp
+        blink = false,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+        match_case = true,
+      },
 
-    -- Optional, customize how note IDs are generated given an optional title.
-    ---@param title string|?
-    ---@return string
-    note_id_func = function(title)
-      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      -- In this case a note with the title 'My new note' will be given an ID that looks
-      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-      local suffix = ""
-      if title ~= nil then
-        -- If title is given, transform it into valid file name.
-        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-      else
-        -- If title is nil, just add 4 random uppercase letters to the suffix.
-        for _ = 1, 4 do
-          suffix = suffix .. string.char(math.random(65, 90))
+      -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+      -- way then set 'mappings = {}'.
+      mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- Toggle check-boxes.
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        -- Smart action depending on context: follow link, show notes with tag, toggle checkbox, or toggle heading fold
+        ["<cr>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        },
+      },
+
+      new_notes_location = "current_dir",
+
+      -- Optional, customize how wiki links are formatted. You can set this to one of:
+      -- _ "use_alias_only", e.g. '[[Foo Bar]]'
+      -- _ "prepend*note_id", e.g. '[[foo-bar|Foo Bar]]'
+      -- * "prepend*note_path", e.g. '[[foo-bar.md|Foo Bar]]'
+      -- * "use_path_only", e.g. '[[foo-bar.md]]'
+      wiki_link_func = "use_alias_only",
+
+      -- Optional, customize how note IDs are generated given an optional title.
+      ---@param title string|?
+      ---@return string
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ""
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
         end
-      end
-      return tostring(os.time()) .. "-" .. suffix
-    end,
+        return tostring(os.time()) .. "-" .. suffix
+      end,
 
-    ---@return table
-    note_frontmatter_func = function(note)
-      if note.title then
-        note:add_alias(note.title)
-      end
-
-      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-
-      -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
+      -- Optional, alternatively you can customize the frontmatter data.
+      ---@return table
+      note_frontmatter_func = function(note)
+        if note.title then
+          note:add_alias(note.title)
         end
-      end
 
-      return out
-    end,
-  },
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        return out
+      end,
+
+      -- Optional, set to true to force ':Obsidian open' to bring the app to the foreground.
+      open_app_foreground = true,
+    }
+
+    vim.keymap.set("n", "<leader>on", "<cmd>Obsidian new<CR>", { desc = "Create New Note" })
+    vim.keymap.set("n", "<leader>oo", "<cmd>Obsidian open<CR>", { desc = "Open in Obsidian" })
+    vim.keymap.set("n", "<leader>os", "<cmd>Obsidian search<CR>", { desc = "Search Obsidian" })
+    vim.keymap.set("n", "<leader>oq", "<cmd>Obsidian quick_switch<CR>", { desc = "Quick Switch" })
+    vim.keymap.set("n", "<leader>ot", "<cmd>Obsidian today<CR>", { desc = "Daily Note" })
+  end,
 }
