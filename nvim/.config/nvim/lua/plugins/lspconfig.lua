bf61e7ab314630b2f3af09e9df394958ed5b837f
spokeyjoe <hq11@rice.edu>
2025-09-24 22:55:31 +0800

    lspconfig: cleanup

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -231,17 +231,10 @@ return {
           semanticHighlighting = true,
         },
       },
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
-      --
+
+      ruff = {},
+
+      pyright = {},
 
       lua_ls = {
         -- cmd = { ... },
@@ -274,7 +267,9 @@ return {
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-      "stylua", -- Used to format Lua code
+      "stylua",
+      "ruff",
+      "black",
     })
     require("mason-tool-installer").setup { ensure_installed = ensure_installed }
 
