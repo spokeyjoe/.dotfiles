9b4025377a59af0ea5c0e9ed5a22d1e320d55769
Joe Qiu <joe.qiu@evas.ai>
2025-12-17 15:52:29 +0800

    gitsigns: update blame function

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -4,13 +4,14 @@ return {
     require("gitsigns").setup {
       on_attach = function(bufnr)
         local gitsigns = require "gitsigns"
+
         local function map(mode, l, r, opts)
           opts = opts or {}
           opts.buffer = bufnr
           vim.keymap.set(mode, l, r, opts)
         end
 
-        map("n", "<leader>gb", gitsigns.toggle_current_line_blame)
+        map("n", "<leader>gb", gitsigns.blame_line, { desc = "[G]it [B]lame line" })
       end,
     }
   end,
