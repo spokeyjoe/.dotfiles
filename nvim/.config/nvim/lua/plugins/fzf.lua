a2d8522cd6e25a79e95eacf3fe04cea26d553864
Joe Qiu <joe.qiu@evas.ai>
2025-12-18 15:59:03 +0800

    add fzf to replace telescope; remove default lsp keymap & reconfigure

---
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,71 @@
+return {
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local fzf = require "fzf-lua"
+    local actions = require "fzf-lua.actions"
+
+    fzf.setup {
+      "default-title",
+
+      ui_select = function(fzf_opts, items)
+        return vim.tbl_deep_extend("force", fzf_opts, {
+          prompt = "Code Actions> ",
+          winopts = {
+            height = 0.33,
+            width = 0.33,
+          },
+        })
+      end,
+
+      actions = {
+        files = {
+          ["default"] = actions.file_edit,
+          ["ctrl-s"] = actions.file_vsplit,
+          ["ctrl-t"] = actions.file_tabedit,
+        },
+        grep = {
+          ["default"] = actions.file_edit,
+          ["ctrl-s"] = actions.file_vsplit,
+        },
+      },
+
+      winopts = {
+        preview = {
+          -- default = "bat",
+          layout = "flex",
+        },
+      },
+    }
+
+    fzf.register_ui_select()
+
+    vim.keymap.set("n", "<leader>sh", fzf.help_tags, { desc = "[S]earch [H]elp" })
+    vim.keymap.set("n", "<leader>sk", fzf.keymaps, { desc = "[S]earch [K]eymaps" })
+    vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
+    vim.keymap.set("n", "<leader>sg", fzf.live_grep, { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>sr", fzf.resume, { desc = "[S]earch [R]esume" })
+    vim.keymap.set("n", "<leader><leader>", fzf.buffers, { desc = "[ ] Find existing buffers" })
+
+    -- Search within the current file's directory
+    vim.keymap.set("n", "<leader>s/", function()
+      local current_file = vim.api.nvim_buf_get_name(0)
+      local search_dir = nil
+      if current_file ~= "" then
+        search_dir = vim.fs.dirname(current_file)
+      end
+
+      fzf.live_grep {
+        cwd = search_dir,
+        prompt = "Live Grep ("
+          .. (search_dir and vim.fn.fnamemodify(search_dir, ":t") or "cwd")
+          .. ")> ",
+      }
+    end, { desc = "[S]earch [/] in Directory" })
+
+    -- Search Neovim configuration files
+    vim.keymap.set("n", "<leader>sn", function()
+      fzf.files { cwd = vim.fn.stdpath "config" }
+    end, { desc = "[S]earch [N]eovim files" })
+  end,
+}
--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -18,19 +18,27 @@ return {
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
         end
 
-        map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition") --  To jump back, press <C-t>.
+        local function safe_del_global(keys)
+          pcall(vim.keymap.del, "n", keys)
+        end
+
+        safe_del_global "grn" -- Rename
+        safe_del_global "gra" -- Code Action
+        safe_del_global "grr" -- References
+        safe_del_global "gri" -- Implementation
+        safe_del_global "grt" -- Type Definition
+
+        local fzf = require "fzf-lua"
+
+        map("gd", fzf.lsp_definitions, "[G]oto [D]efinition") --  To jump back, press <C-t>.
         map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-        map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-        map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+        map("gr", fzf.lsp_references, "[G]oto [R]eferences")
+        map("gI", fzf.lsp_implementations, "[G]oto [I]mplementation")
         map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
         map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-        map("<leader>T", require("telescope.builtin").lsp_type_definitions, "[T]ype Definition")
-        map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-        map(
-          "<leader>ws",
-          require("telescope.builtin").lsp_dynamic_workspace_symbols,
-          "[W]orkspace [S]ymbols"
-        )
+        map("<leader>T", fzf.lsp_typedefs, "[T]ype Definition")
+        map("<leader>ds", fzf.lsp_document_symbols, "[D]ocument [S]ymbols")
+        map("<leader>ws", fzf.lsp_live_workspace_symbols, "[W]orkspace [S]ymbols")
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
