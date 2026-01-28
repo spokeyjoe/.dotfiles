9cbe14b4ac5c516383a727aa508b47f443a7503a
Joe Qiu <joe.qiu@evas.ai>
2026-01-28 16:09:42 +0800

    fish: add copy function

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -73,6 +73,22 @@ alias tk='tmux kill-session -t'
 alias tka='tmux kill-server'
 alias tm='tmux attach-session -t main 2>/dev/null || tmux new-session -s main'
 
+function copy --description 'Copy input to client clipboard using OSC 52'
+    if count $argv > /dev/null
+        set input (cat $argv)
+    else
+        set input (cat)
+    end
+
+    set -l b64 (echo -n "$input" | base64 | string join '')
+
+    if set -q TMUX
+        printf "\ePtmux;\e\e]52;c;%s\a\e\\" "$b64"
+    else
+        printf "\e]52;c;%s\a" "$b64"
+    end
+end
+
 # Proxy
 function proxy
     echo "🌐 Select Proxy Profile:"
