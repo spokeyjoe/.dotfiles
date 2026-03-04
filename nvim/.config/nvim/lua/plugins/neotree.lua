7698b2521e2f2edbd23550fbef0e612346671ad0
Joe Qiu <joe.qiu@evas.ai>
2026-03-04 14:18:26 +0800

    neotree: copy path keymap

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -11,6 +11,41 @@ return {
     require("neo-tree").setup {
       close_if_last_window = true,
       use_libuv_file_watcher = true,
+      filesystem = {
+        commands = {
+          copy_selector = function(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local vals = {
+              ["1"] = filepath,
+              ["2"] = modify(filepath, ":."),
+              ["3"] = filename,
+            }
+
+            local options = {
+              "1. Absolute Path: " .. vals["1"],
+              "2. Relative Path: " .. vals["2"],
+              "3. Filename:      " .. vals["3"],
+            }
+
+            vim.ui.select(options, { prompt = "Copy to Clipboard" }, function(choice)
+              if choice then
+                local i = choice:sub(1, 1)
+                vim.fn.setreg("+", vals[i])
+                vim.notify("Copied: " .. vals[i])
+              end
+            end)
+          end,
+        },
+        window = {
+          mappings = {
+            ["Y"] = "copy_selector",
+          },
+        },
+      },
     }
     vim.keymap.set(
       "n",
